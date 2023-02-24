ea156fca67893c75e4494104e1ca31d353f24a59
TingYinHelen <yinhelen.hlj@qq.com>
2023-02-24 22:28:46 +0800

    添加注释

--- ExtraStorage.sol
+++ ExtraStorage.sol
@@ -3,8 +3,9 @@
 pragma solidity 0.8.7;
 
 import "./SimpleStorage.sol";
-
+// 使用is指定继承关系
 contract ExtraStorage is SimpleStorage {
+  // 重写store函数，还需要在SimpleStorage的store函数加上virtual修饰符
     function store(uint256 _favoriteNumber) public override {
         favoriteNumber = _favoriteNumber + 5;
     }
--- StorageFactory.sol
+++ StorageFactory.sol
@@ -1,7 +1,10 @@
 // SPDX-License-Identifier: MIT 
 
 pragma solidity ^0.8.7;
-
+// 现在编译版本是0.8的任意版本都可用。而SimpleStorage的是^0.8.8，那么是可以编译通过的，
+// 如果将StorageFactor的版本改为^0.7.0，编译就会报错，因为StorageFactor是0.7版本的任意版本都可以用
+// 而Simplestore是0.8.8以上，那么这两个版本不兼容，所以两个编译版本需要兼容
+// 这里可以指定其他文件的路径，也可以指定其他包或者Github的路径
 import "./SimpleStorage.sol"; 
 
 contract StorageFactory {
@@ -9,6 +12,7 @@ contract StorageFactory {
     SimpleStorage[] public simpleStorageArray;
     
     function createSimpleStorageContract() public {
+      // 使用new方法就可以部署合约
         SimpleStorage simpleStorage = new SimpleStorage();
         simpleStorageArray.push(simpleStorage);
     }
@@ -25,3 +29,10 @@ contract StorageFactory {
         return simpleStorageArray[_simpleStorageIndex].retrieve();
     }
 }
+// 这里查看simpleStorageArr，可以看到部署后合约的地址
+
+// 与其他合约交互需要两样东西，1. 合约地址，2. 合约ABI
+// Address：这里我们把地址存在了simpleStorageArr里
+// ABI: 是指应用程序二进制接口，ABI会告诉我们的代码如何来跟合约交互，只要我们导入的合约，就能获取他的ABI
+
+
